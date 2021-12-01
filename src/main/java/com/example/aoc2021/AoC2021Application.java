8fa7137930523f2d4c5e45eb7dba9d6de8c2b61a
cfaucher <cedric@ordigraphe.com>
2021-11-30 20:08:16 -0500

    initial commit

---
+++ src/main/java/com/example/aoc2021/AoC2021Application.java
@@ -0,0 +1,13 @@
+package com.example.aoc2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoC2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoC2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aoc2021/AoC2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aoc2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoC2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
